050d39bf7d023b6bfd8af2b361f8e4b76309f354
Muhammed Aly <75846623+mohammedaly915@users.noreply.github.com>
2023-06-12 14:53:34 +0300

    Add files via upload

--- src/form_react/REgistration/Registration.js
+++ src/form_react/REgistration/Registration.js
@@ -1,101 +1,109 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import {useNavigate } from 'react-router-dom'
 //import { signIN, signup } from '../../Home'
 import {  Container, Form, Toast } from 'react-bootstrap'
 function Registration() {
     
-    const api="http://localhost:9000/user"
+    const api="https://productdp-aksv.vercel.app/user"
     const [id,setId]=useState("")
     const [name,setName]=useState("")
     const [pass,setPassword]=useState("")
     const [email,setEmail]=useState("")
-    //const Navigte = useNavigate()
+    let ErrorMsg=null
+    let [validate,setvalidate]=useState(true)
+    const Navigate = useNavigate()
 
     const IsValide=()=>{
-        let proCess =true;
-        let ErrorMsg="please Enter "
+        let process =true;
+        ErrorMsg="please Enter "
         if (id === null || id ===''){
-            ErrorMsg+="Username"
-            proCess=false
+            ErrorMsg+="Username "
+            
+            process=false
         }
         if (name === null || name ===''){
-            ErrorMsg+="Name"
-            proCess=false
+            ErrorMsg+="FullName "
+            process=false
+            
         }
         if (email === null || email ===''){
-            ErrorMsg+="email"
-            proCess=false
+            ErrorMsg+="email "
+            process=false
+        
         }
         if (pass === null || pass ===''){
-            ErrorMsg+="Password"
-            proCess=false
+            ErrorMsg+="Password "
+            process=false
         }
-        if (!proCess){
-            console.log('warning'+ErrorMsg);
-        
-        }else{
+        if (!process){
+            console.log('warning '+ErrorMsg);
+            }else{
             if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email))
             {
 
             }else{
-                proCess=false;
-                console.log("failed email");
+                process=false;
+                console.log("failed email ");
                 
             }
         }
-        return proCess;
+        return process;
     }
-
+    
     let user=[];
     const registerhandel=(e)=>{
         e.preventDefault();
-        (<Toast className="d-inline-block m-1" bg={`Danger`.toLowerCase()} key={1}>
-        <Toast.Body className="Danger">Hello, world! This is a toast message.</Toast.Body>
-        </Toast>)
+       
         let data={id,name,pass,email}
         console.log(data);
         user.push(data)
         console.log(user);
         if(IsValide()){
-        fetch(api,{
-        method:"post",
-        headers:{'content-type':' application/json'},
-        body:JSON.stringify(data)
-    }).then((res)=>{
-        console.log("upload");
-        
-    }).catch((err)=>{
-        console.log("failed ublosd");
-        <Toast className="d-inline-block m-1" bg={`Danger`.toLowerCase()} key={1}>
-        <Toast.Body className="Danger">Hello, world! This is a toast message.</Toast.Body>
-        </Toast>
-    }) 
+            fetch(api,{
+            method:"post",
+            headers:{'content-type':' application/json'},
+            body:JSON.stringify(data)
+        }).then((res)=>{
+            console.log("upload and data posted");
+            Navigate('/login')
+            
+        }).catch((err)=>{
+            console.log(" failed ubload and data unposted");
+        }) 
     }}
 
     
-      //()=>signup===true?signIN=true:null
+
   return (<> 
     
-    <Container>
+    <Container className="log-page"> 
         <h1 className='py-5' >Register</h1>
         <Form  onSubmit={registerhandel}>
+        {
+        IsValide()===false?
+        (<Toast className="d-inline-block m-1" bg={`Danger`.toLowerCase()} key={1}>
+        <Toast.Body className="Danger"></Toast.Body>
+        </Toast>):(<Toast className="d-inline-block m-1" bg={`Success`.toLowerCase()} key={1}>
+        <Toast.Body className="Success"></Toast.Body>
+        </Toast>)     
+    }
         <Form.Group className="mb-3" controlId={`formBasicname`}>
                 <Form.Label>Full Name</Form.Label>
-                <Form.Control  type='text' required onChange={(e)=>setName(e.target.value)} placeholder="Enter Name" />
+                <Form.Control  type='text'  onChange={(e)=>setName(e.target.value)} placeholder="Enter Name" />
             </Form.Group>
         <Form.Group className="mb-3" controlId={`formBasicUser`}>
                 <Form.Label>User Name</Form.Label>
-                <Form.Control  type='text' required onChange={(e)=>setId(e.target.value)} placeholder="Enter User" />
+                <Form.Control  type='text'  onChange={(e)=>setId(e.target.value)} placeholder="Enter User" />
             </Form.Group>
         <Form.Group className="mb-3" controlId={`formBasicemail`}>
                 <Form.Label>email</Form.Label>
-                <Form.Control type='email' required onChange={(e)=>setEmail(e.target.value)} placeholder="Enter email" />
+                <Form.Control type='text'  onChange={(e)=>setEmail(e.target.value)} placeholder="Enter email" />
             </Form.Group>
         <Form.Group className="mb-3" controlId={`formBasicpassword`}>
                 <Form.Label>password</Form.Label>
                 <Form.Control type='password'  onChange={(e)=>setPassword(e.target.value)} placeholder="Enter password" />
             </Form.Group>
-            <input type="submit" value="Register"/>
+            <input type="submit" className="log_btn" value="Register"/>
             </Form>
     </Container>
   </>
